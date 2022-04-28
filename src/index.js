ba9c5a4c44e9c1b54bb8f41ce5d8e7573c064e0f
Haji Bamsi <hibamsi@gmail.com>
2022-04-28 16:04:59 +0300

    add validation to form input

--- src/index.js
+++ src/index.js
@@ -2,31 +2,33 @@
 /* eslint-disable */
 import _ from "lodash";
 /* eslint-enable */
-import './style.css';
-import { addListItem } from './modules/add.module.js';
-import { loadData } from './modules/load-data.module.js';
-import { update } from './modules/update.module.js';
-import { removeItems } from './modules/remove.module.js';
+import "./style.css";
+import { addListItem } from "./modules/add.module.js";
+import { loadData } from "./modules/load-data.module.js";
+import { update } from "./modules/update.module.js";
+import { removeItems } from "./modules/remove.module.js";
 
-const newItem = document.querySelector('#add-list');
+const newItem = document.querySelector("#add-list");
 let deleteList = [];
 
 window.onload = loadData();
 
-newItem.addEventListener('click', () => {
-  const description = document.getElementById('description');
-  addListItem(description.value);
-  description.value = '';
-  window.location = 'index.html';
+newItem.addEventListener("click", () => {
+  const description = document.getElementById("description");
+  if (description.value !== "") {
+    addListItem(description.value);
+    description.value = "";
+    window.location = "index.html";
+  }
 });
 
-const links = document.querySelectorAll('.list-desc');
+const links = document.querySelectorAll(".list-desc");
 
 function hideAll() {
   links.forEach((e) => {
-    const item = e.getAttribute('id');
+    const item = e.getAttribute("id");
     const input = document.querySelector(item);
-    input.classList.add('hide');
+    input.classList.add("hide");
   });
 }
 
@@ -35,8 +37,8 @@ function deleteItem(index) {
   const tick = `tick_${index}`;
   const inactiveItem = document.getElementById(square);
   const activeItem = document.getElementById(tick);
-  inactiveItem.style.display = 'none';
-  activeItem.style.display = 'inline';
+  inactiveItem.style.display = "none";
+  activeItem.style.display = "inline";
   deleteList.push(index);
 }
 
@@ -45,39 +47,39 @@ function activateItem(index) {
   const tick = `tick_${index}`;
   const activeItem = document.getElementById(square);
   const inactiveItem = document.getElementById(tick);
-  inactiveItem.style.display = 'none';
-  activeItem.style.display = 'inline';
+  inactiveItem.style.display = "none";
+  activeItem.style.display = "inline";
   deleteList = deleteList.filter((item) => item !== index);
 }
 
 links.forEach((e) => {
-  e.addEventListener('click', () => {
-    e.classList.add('hide');
-    const item = e.getAttribute('id');
+  e.addEventListener("click", () => {
+    e.classList.add("hide");
+    const item = e.getAttribute("id");
     const input = document.querySelector(item);
-    input.classList.remove('hide');
-    input.addEventListener('focusout', () => {
-      const index = item.split('_')[1];
+    input.classList.remove("hide");
+    input.addEventListener("focusout", () => {
+      const index = item.split("_")[1];
       const newItem = {
         id: index,
         description: input.value,
       };
       update(newItem);
-      e.classList.remove('hide');
+      e.classList.remove("hide");
       hideAll();
-      window.location = 'index.html';
+      window.location = "index.html";
     });
   });
 });
 
-const checkedItem = document.querySelectorAll('.item-check');
+const checkedItem = document.querySelectorAll(".item-check");
 
 checkedItem.forEach((e) => {
-  e.addEventListener('click', () => {
-    const item = e.getAttribute('id');
-    const arr = item.split('_');
+  e.addEventListener("click", () => {
+    const item = e.getAttribute("id");
+    const arr = item.split("_");
     const index = arr[1];
-    if (arr[0] === 'square') {
+    if (arr[0] === "square") {
       deleteItem(parseInt(index, 10));
     } else {
       activateItem(parseInt(index, 10));
@@ -85,9 +87,9 @@ checkedItem.forEach((e) => {
   });
 });
 
-const clearItem = document.getElementById('clear-items');
-clearItem.addEventListener('click', () => {
+const clearItem = document.getElementById("clear-items");
+clearItem.addEventListener("click", () => {
   removeItems(deleteList);
   deleteList = [];
-  window.location = 'index.html';
+  window.location = "index.html";
 });
