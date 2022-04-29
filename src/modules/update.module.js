5f24f95b3174c70bdaabd2c7b6337d1d06a2ae0f
Haji Bamsi <hibamsi@gmail.com>
2022-04-29 12:21:51 +0300

    update javascript module

--- src/modules/update.module.js
+++ src/modules/update.module.js
@@ -1,10 +1,11 @@
 /* eslint "import/prefer-default-export":"off" */
-import { readLocalStorage, writeLocalStorage } from './local-storage.module.js';
+import { readLocalStorage, writeLocalStorage } from "./local-storage.module.js";
 
 function update(item) {
   const items = readLocalStorage();
   const element = items.filter((i) => i.id === item.id)[0];
   element.description = item.description;
+  element.completed = item.completed;
   items[item.id - 1] = element;
   writeLocalStorage(items);
 }
