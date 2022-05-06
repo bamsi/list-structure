d7b144a085f2ebc8d6a88061615d4964dbbbf925
AbuRayhaan <91697183+AbuRayhaan@users.noreply.github.com>
2022-05-06 14:10:23 +0100

    Modified the complete.module.js file

--- src/modules/completed.module.js
+++ src/modules/completed.module.js
@@ -1,12 +1,12 @@
 /* eslint "import/prefer-default-export":"off" */
 import { readLocalStorage, writeLocalStorage } from './local-storage.module.js';
 
-function update(item) {
+function completed(index, status) {
   const items = readLocalStorage();
-  const element = items.filter((i) => i.id === item.id)[0];
-  element.description = item.description;
-  items[item.id - 1] = element;
+  const element = items.filter((i) => i.id === index)[0];
+  element.completed = status;
+  items[index - 1] = element;
   writeLocalStorage(items);
 }
 
-export { update };
+export { completed };
---
+++ src/modules/completed.module.test.js
@@ -0,0 +1,13 @@
+import { completed } from "./completed.module.js";
+import { addListItem } from "./add.module.js";
+import { readLocalStorage } from "./local-storage.module.js";
+jest.mock('./local-storage.module');
+
+describe('should update completed status', function(){
+    test('update completed status', ()=>{
+        addListItem('new item added');
+       completed(1, true);
+       const item = readLocalStorage();
+       expect(item[0].completed).toBeTruthy();
+    });
+});
